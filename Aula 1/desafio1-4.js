63efa672a780e2ee213fdcef713e0de11070b5fe
andrebonfim <a.bonfim08@gmail.com>
2020-07-22 22:06:14 -0300

    Added getAverageTransactionValue and getTransactionsCount function

--- Aula 1/desafio1-4.js
+++ Aula 1/desafio1-4.js
@@ -9,28 +9,20 @@ const user = {
 };
 
 // Atualiza as transações do usuário
-createTransaction({type: 'credit', value: 50.5})
-createTransaction({type: 'credit', value: 80})
-createTransaction({type: 'debit', value: 40})
-createTransaction({type: 'debit', value: 30})
+createTransaction({ type: 'credit', value: 50.5 })
+createTransaction({ type: 'credit', value: 80 })
+createTransaction({ type: 'debit', value: 40 })
+createTransaction({ type: 'debit', value: 30 })
 
-// Atualiza o balance
-updateBalance()
-
-// Retorna o maior valor de credit ou debit
-// const credit = user.transactions[0].type === 'credit'
-
-const credit = getHigherTransactionByType('credit')
-
-console.log(credit)
-
+// Enviar as transações para o array transaction de user
 function createTransaction(transaction) {
   user.transactions.push(transaction)
 }
 
+// Atualizar o Balance
 function updateBalance() {
-  for(let transaction of user.transactions){
-    if(transaction.type === 'credit') {
+  for (let transaction of user.transactions) {
+    if (transaction.type === 'credit') {
       user.balance += transaction.value // += é a mesma coisa que ter user.balance = user.balance + transaction.value;
     } else {
       user.balance -= transaction.value
@@ -38,24 +30,61 @@ function updateBalance() {
   }
 }
 
-// Crie uma função chamada getHigherTransactionByType que recebe como parâmetro o tipo de transação credit/debit
-// function getHigherTransactionByType() {
-//   for(transaction of user.transactions) {
-//     if(transaction.type === 'credit') {
-//       console.log(Math.max(transaction.value))
-//     }
-//   }
-// }
+updateBalance()
+console.log(`O saldo atual é de: ${user.balance}`)
 
+// Retornar maior valor
 function getHigherTransactionByType(type) {
-  let higherValue = 0;
-  let higherTransaction;
+  let higherValue = 0
+  let higherTransaction
 
-  for(transaction of user.transactions) {
-    if(transaction.type === type && transaction.value > higherValue) {
-      higherValue += transaction.value; 
-      higherTransaction = transaction.value;
+  for (let transaction of user.transactions) {
+    if (transaction.type === type && transaction.value > higherValue) {
+      higherValue += transaction.value
+      higherTransaction = transaction.value
     }
   }
   return higherTransaction;
 }
+
+const highCredit = getHigherTransactionByType('credit')
+const highDebit = getHigherTransactionByType('debit')
+
+console.log(`O maior valor de crédito é: ${highCredit}`)
+console.log(`O maior valor de débito é: ${highDebit}`)
+
+// Retornar o valor médio de todas as transações
+function getAverageTransactionValue() {
+  let sum = 0
+
+  for (let transaction of user.transactions) {
+    sum += transaction.value
+
+  }
+  const result = sum / user.transactions.length
+  const media = result.toFixed(2)
+
+  return media
+}
+
+const media = getAverageTransactionValue()
+
+console.log(`A média de todas as transações é: ${media}`)
+
+// Apresenta o total de transações para credit e para debit
+function getTransactionsCount() {
+  let totalCredit = 0
+  let totalDebit = 0
+
+  for (let transaction of user.transactions) {
+    if(transaction.type === 'credit') {
+      totalCredit ++
+    } else {
+      totalDebit ++
+    }
+  }
+  return {'Total de Crédito': totalCredit, 'Total de Débito': totalDebit}
+}
+
+const total = getTransactionsCount()
+console.table(total)
