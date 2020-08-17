98170a3069e4ecfc9307cf39ab4b390c9b1869cf
andrebonfim <a.bonfim08@gmail.com>
2020-08-17 20:52:50 -0300

    Add function maximaze-minimize modal

--- Aula 2/desafios/scripts/scripts.js
+++ Aula 2/desafios/scripts/scripts.js
@@ -4,10 +4,10 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards) {
   card.addEventListener('click', function(){
-    const videoId = card.getAttribute('id')
+    const pageId = card.getAttribute('id')
 
     modalOverlay.classList.add('active')
-    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${videoId}`
+    modalOverlay.querySelector('iframe').src = `https://app.rocketseat.com.br/${pageId}`
   })
 }
 
@@ -18,6 +18,28 @@ document.querySelector('.close-modal').addEventListener('click', function() {
 })
 
 // MAXIMIZE MODAL
-document.querySelector('.maximize-modal').addEventListener('click', function() {
-  modal.classList.contains('maximize')
-})
+const elem = document.querySelector(".modal-overlay");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+    modal.classList.add('fullscreen')
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
