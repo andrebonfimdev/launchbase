5e32588108fb083ec98eb3a54e5aaf8a537d6dae
andrebonfim <a.bonfim08@gmail.com>
2020-08-27 19:42:46 -0300

    Developing first part of mobile

---
+++ modulo03/DESAFIOS/public/scripts/menu-mobile.js
@@ -0,0 +1,16 @@
+function menuMobile() {
+  document.getElementById("menu-list-ul").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.menu-btn')) {
+    const dropdowns = document.getElementsByClassName("menu-list-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
