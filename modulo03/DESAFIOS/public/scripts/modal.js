61fce8aae4c10efb8967d72d1032d81bce728de4
andrebonfim <a.bonfim08@gmail.com>
2020-08-29 23:20:47 -0300

    Adjusted various properties for web layout

--- modulo03/DESAFIOS/public/scripts/modal.js
+++ modulo03/DESAFIOS/public/scripts/modal.js
@@ -5,9 +5,9 @@ const cards = document.querySelectorAll('.card')
 for(let card of cards) {
   card.addEventListener('click', function(){
     const pageId = card.getAttribute('id')
-
+    console.log(pageId)
     modalOverlay.classList.add('active')
-    modalOverlay.querySelector('iframe').src = `https://app.rocketseat.com.br/${pageId}`
+    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`
   })
 }
 
